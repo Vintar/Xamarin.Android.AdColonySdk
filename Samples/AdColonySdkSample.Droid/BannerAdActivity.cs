5ecef6fa5a1de2c82304c39f71e7963687d232b1
Krzus Michał <michal.krzus@iteo.com>
2020-03-11 18:30:19 +0100

    4.1.4 - Init

---
+++ Samples/AdColonySdkSample.Droid/BannerAdActivity.cs
@@ -0,0 +1,129 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Com.Adcolony.Sdk;
+
+namespace AdColonySdkSample.Droid
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme")]
+    public class BannerAdActivity : AppCompatActivity
+    {
+        private const string APP_ID = "app185a7e71e1714831a49ec7";
+        private const string ZONE_ID = "vz785bc8d42d9c43fdaf";
+        private const string TAG = "AdColonyBannerDemo";
+
+        public AdColonyAdViewListener listener;
+        public AdColonyAdOptions adOptions;
+        public RelativeLayout adContainer;
+        public ProgressBar progressBar;
+        public Button buttonLoad;
+        public AdColonyAdView adView;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_banner_ad);
+
+            adContainer = FindViewById<RelativeLayout>(Resource.Id.ad_container);
+            progressBar = FindViewById<ProgressBar>(Resource.Id.progress);
+            buttonLoad = FindViewById<Button>(Resource.Id.load_button);
+            buttonLoad.Click += ButtonLoad_Click;
+
+            // Construct optional app options object to be sent with configure
+            AdColonyAppOptions appOptions = new AdColonyAppOptions();
+
+            // Configure AdColony in your launching Activity's onCreate() method so that cached ads can
+            // be available as soon as possible.
+            AdColony.Configure(this, appOptions, APP_ID, ZONE_ID);
+
+            listener = new MyAdColonyAdViewListener(this);
+
+            // GDPR
+            AdColony.AppOptions.SetGDPRConsentString("0");
+            AdColony.AppOptions.SetGDPRRequired(false);
+        }
+
+        private void ButtonLoad_Click(object sender, System.EventArgs e)
+        {
+            //Remove previous ad view if present.
+            if (adContainer.ChildCount > 0)
+            {
+                adContainer.RemoveView(adView);
+            }
+
+            progressBar.Visibility = ViewStates.Visible;
+            buttonLoad.Enabled = false;
+            buttonLoad.Alpha = 0.5f;
+
+            RequestBannerAd();
+        }
+
+        private void RequestBannerAd()
+        {
+            // Optional Ad specific options to be sent with request
+            adOptions = new AdColonyAdOptions();
+
+            //Request Ad
+            AdColony.RequestAdView(ZONE_ID, listener, AdColonyAdSize.Banner, adOptions);
+        }
+
+        private void ResetUI()
+        {
+            progressBar.Visibility = ViewStates.Gone;
+            buttonLoad.Enabled = true;
+            buttonLoad.Alpha = 1.0f;
+        }
+
+        private class MyAdColonyAdViewListener : AdColonyAdViewListener
+        {
+            private readonly BannerAdActivity _activity;
+
+            public MyAdColonyAdViewListener(BannerAdActivity activity)
+            {
+                _activity = activity;
+            }
+
+            public override void OnRequestFilled(AdColonyAdView adColonyAdView)
+            {
+                Log.Debug(TAG, "onRequestFilled");
+                _activity.ResetUI();
+                _activity.adContainer.AddView(adColonyAdView);
+                _activity.adView = adColonyAdView;
+            }
+
+            public override void OnRequestNotFilled(AdColonyZone zone)
+            {
+                base.OnRequestNotFilled(zone);
+                Log.Debug(TAG, "onRequestNotFilled");
+                _activity.ResetUI();
+            }
+
+            public override void OnOpened(AdColonyAdView ad)
+            {
+                base.OnOpened(ad);
+                Log.Debug(TAG, "onOpened");
+            }
+
+            public override void OnClosed(AdColonyAdView ad)
+            {
+                base.OnClosed(ad);
+                Log.Debug(TAG, "onClosed");
+            }
+
+            public override void OnClicked(AdColonyAdView ad)
+            {
+                base.OnClicked(ad);
+                Log.Debug(TAG, "onClicked");
+            }
+
+            public override void OnLeftApplication(AdColonyAdView ad)
+            {
+                base.OnLeftApplication(ad);
+                Log.Debug(TAG, "onLeftApplication");
+            }
+        }
+    }
+}
---
+++ Samples/AdColonySdkSample.Droid/InterstitialAdActivity.cs
@@ -0,0 +1,131 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Com.Adcolony.Sdk;
+
+namespace AdColonySdkSample.Droid
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme")]
+    public class InterstitialAdActivity : AppCompatActivity
+    {
+        private const string APP_ID = "app185a7e71e1714831a49ec7";
+        private const string ZONE_ID = "vz06e8c32a037749699e7050";
+        private const string TAG = "AdColonyDemo";
+
+        public Button showButton;
+        public ProgressBar progress;
+        public AdColonyInterstitial ad;
+        public AdColonyAdOptions adOptions;
+        public AdColonyInterstitialListener listener;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_interstitial_ad);
+
+            progress = FindViewById<ProgressBar>(Resource.Id.progress);
+
+            // Construct optional app options object to be sent with configure
+            AdColonyAppOptions appOptions = new AdColonyAppOptions()
+                .SetUserID("unique_user_id")
+                .SetKeepScreenOn(true);
+
+            // Configure AdColony in your launching Activity's onCreate() method so that cached ads can
+            // be available as soon as possible.
+            AdColony.Configure(this, appOptions, APP_ID, ZONE_ID);
+
+            // Optional user metadata sent with the ad options in each request
+            AdColonyUserMetadata metadata = new AdColonyUserMetadata()
+                .SetUserAge(26)
+                .SetUserEducation(AdColonyUserMetadata.UserEducationBachelorsDegree)
+                .SetUserGender(AdColonyUserMetadata.UserMale);
+
+            // Ad specific options to be sent with request
+            adOptions = new AdColonyAdOptions().SetUserMetadata(metadata);
+
+            // Set up listener for interstitial ad callbacks. You only need to implement the callbacks
+            // that you care about. The only required callback is onRequestFilled, as this is the only
+            // way to get an ad object.
+            listener = new MyAdColonyInterstitialListener(this);
+
+            // Set up button to show an ad when clicked
+            showButton = FindViewById<Button>(Resource.Id.showbutton);
+            showButton.Click += ShowButton_Click;
+
+            // GDPR
+            AdColony.AppOptions.SetGDPRConsentString("0");
+            AdColony.AppOptions.SetGDPRRequired(false);
+        }
+
+        private void ShowButton_Click(object sender, System.EventArgs e)
+        {
+            ad.Show();
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+            // It's somewhat arbitrary when your ad request should be made. Here we are simply making
+            // a request if there is no valid ad available onResume, but really this can be done at any
+            // reasonable time before you plan on showing an ad.
+            if (ad == null || ad.IsExpired)
+            {
+                // Optionally update location info in the ad options for each request:
+                // LocationManager locationManager =
+                //     (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                // Location location =
+                //     locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                // adOptions.setUserMetadata(adOptions.getUserMetadata().setUserLocation(location));
+                progress.Visibility = ViewStates.Visible;
+                AdColony.RequestInterstitial(ZONE_ID, listener, adOptions);
+            }
+        }
+
+        private class MyAdColonyInterstitialListener : AdColonyInterstitialListener
+        {
+            private readonly InterstitialAdActivity _activity;
+
+            public MyAdColonyInterstitialListener(InterstitialAdActivity activity)
+            {
+                _activity = activity;
+            }
+
+            public override void OnRequestFilled(AdColonyInterstitial ad)
+            {
+                // Ad passed back in request filled callback, ad can now be shown
+                _activity.ad = ad;
+                _activity.showButton.Enabled = true;
+                _activity.progress.Visibility = ViewStates.Invisible;
+                Log.Debug(TAG, "onRequestFilled");
+            }
+
+            public override void OnRequestNotFilled(AdColonyZone zone)
+            {
+                // Ad request was not filled
+                _activity.progress.Visibility = ViewStates.Invisible;
+                Log.Debug(TAG, "onRequestNotFilled");
+            }
+
+            public override void OnOpened(AdColonyInterstitial ad)
+            {
+                // Ad opened, reset UI to reflect state change
+                _activity.showButton.Enabled = false;
+                _activity.progress.Visibility = ViewStates.Visible;
+                Log.Debug(TAG, "onOpened");
+            }
+
+            public override void OnExpiring(AdColonyInterstitial ad)
+            {
+                // Request a new ad if ad is expiring
+                _activity.showButton.Enabled = false;
+                _activity.progress.Visibility = ViewStates.Visible;
+                AdColony.RequestInterstitial(ZONE_ID, this, _activity.adOptions);
+                Log.Debug(TAG, "onExpiring");
+            }
+        }
+    }
+}
